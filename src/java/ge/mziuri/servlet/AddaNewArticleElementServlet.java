e3bb3a223995eb40c0fd9c922e4bd19ea554b457
tatopk <tatoleto12345@gmail.com>
2017-05-06 17:28:26 +0400

    working on articleelementservlet

--- src/java/ge/mziuri/servlet/AddaNewArticleElementServlet.java
+++ src/java/ge/mziuri/servlet/AddaNewArticleElementServlet.java
@@ -1,22 +1,36 @@
 package ge.mziuri.servlet;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 
+@WebServlet("/Relay")
+@MultipartConfig
 public class AddaNewArticleElementServlet extends HttpServlet {
-    
-    private String imagePath;
-    private String filePath;
 
-//    private String name;
-   // private String cookingway;
+    private boolean isMultipart;
+    private String filePath;
+    private int maxFileSize = 5000 * 1024;
+    private int maxMemSize = 5000 * 1024;
+    private File file;
+    private String imagePath;
+
+    @Override
     public void init() {
         // Get the file location where it would be stored.
-        filePath
-                = getServletContext().getInitParameter("file-upload");
+        filePath = getServletContext().getInitParameter("file-upload");
     }
 
     @Override
@@ -26,7 +40,68 @@ public class AddaNewArticleElementServlet extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        String text = request.getParameter("text");
-        String picture = request.getParameter("picture");
+        try {
+
+            isMultipart = ServletFileUpload.isMultipartContent(request);
+            response.setContentType("text/html");
+
+            DiskFileItemFactory factory = new DiskFileItemFactory();
+            // maximum size that will be stored in memory
+            factory.setSizeThreshold(maxMemSize);
+            // Location to save data that is larger than maxMemSize.
+            factory.setRepository(new File("c:\\temp"));
+
+            // Create a new file upload handler
+            ServletFileUpload upload = new ServletFileUpload(factory);
+            // maximum file size to be uploaded.
+            upload.setSizeMax(maxFileSize);
+
+            // Parse the request to get file items.
+            List fileItems = upload.parseRequest(request);
+
+            // Process the uploaded file items
+            Iterator i = fileItems.iterator();
+            Random random = new Random();
+
+            while (i.hasNext()) {
+                FileItem fi = (FileItem) i.next();
+                if (!fi.isFormField()) {
+
+                    String fieldName = fi.getName() + " " + fi.getFieldName();
+                    String fileName = fi.getName();
+                    String contentType = fi.getContentType();
+                    boolean isInMemory = fi.isInMemory();
+                    long sizeInBytes = fi.getSize();
+
+                    String extention = FilenameUtils.getExtension(fileName);
+                    String imageName = FilenameUtils.removeExtension(fileName);
+
+                    if (fileName.lastIndexOf("\\") >= 0) {
+                        fileName = imageName + random.nextInt() + "." + extention;
+                        imagePath = fileName;
+
+                        file = new File(filePath
+                                + fileName.substring(fileName.lastIndexOf("\\")));
+
+                    } else {
+
+                        fileName = imageName + random.nextInt() + "." + extention;
+                        imagePath = fileName;
+
+                        file = new File(filePath
+                                + fileName.substring(fileName.lastIndexOf("\\") + 1));
+
+                    }
+                    try {
+                        fi.write(file);
+                    } catch (Exception ex) {
+                        System.out.println(ex.getMessage());
+                    }
+
+                }
+            }
+        } catch (FileUploadException  ex) {
+
+        }
     }
 }
--- web/ArticleElement.jsp
+++ web/ArticleElement.jsp
@@ -8,33 +8,26 @@
         <title>Article Page</title>
     </head>
     <body>
-        <h1 id="TopText">Add your content<h1>
+          <h1 id="TopText">Add your content<h1>
                 <hr>
                 <form action="AddaNewArticleElementServlet" method="post" enctype="multipart/form-data">
                     <textarea rows="10" cols="100"></textarea>
 
-                    <input id="inp2" type="file" accept="image/*" onchange="loadFile(event)">
-                    <img id="inp1"/>
+                    <input id="inp2" type="file" accept="image/*" onchange="loadFile(event)"/>
+                    <img id="inp1" src="Resources/No_Image.webp">
                     <script>
+                        var output = document.getElementById('inp1');
+                        output.setAttribute("top", "100px");
+                        if (output && output.style) {
+                            output.style.height = '200px';
+                            output.style.width = '200px';
+                            output.style.top = "190px";
+                            output.style.left = "561px";
+                        }
                         var loadFile = function (event) {
                             var output = document.getElementById('inp1');
                             output.src = URL.createObjectURL(event.target.files[0]);
                         };
-
-//                        var img = document.getElementById("inp1");
-//
-//                        if (img.complete) {
-//                            var x = document.createElement("TEXTAREA");
-//                            document.body.appendChild(x);
-//                            x.setAttribute("style", "position:absolute; left:" + 280 + "px; top:" + 200 + "px");
-//                            x.setAttribute("rows", "10");
-//                            x.setAttribute("cols", "100");
-//                        } else {
-//                            img.addEventListener('load', loaded);
-//                            img.addEventListener('error', function () {
-//                                alert('error');
-//                            });
-//                        }
                     </script>
                     <button id="submitbutton" type="submit">Submit</button>
                 </form>
